8c93d9a02cbfc84bba7153958d8bda0c9659bc19
onfranciis <onukwuf@gmail.com>
2025-06-20 11:32:35 +0100

    chore: remove old migration file

--- prisma/schema/migrations/20250619202052_ten/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `ConferenceSummit` MODIFY `otherRole` VARCHAR(191) NULL;
--- prisma/schema/migrations/20250619205506_eleven/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `state` to the `SpeakerApplication` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `SpeakerApplication` ADD COLUMN `city` VARCHAR(191) NULL,
-    ADD COLUMN `state` VARCHAR(191) NOT NULL;
