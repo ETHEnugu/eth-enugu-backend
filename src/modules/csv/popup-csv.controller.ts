08bb3dd312b945567aaab1ae7178ff0032e6e313
Francis Onukwu <onukwuf@gmail.com>
2025-07-01 17:04:24 +0100

    feat: add csv feature

---
+++ src/modules/csv/popup-csv.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import { Controller } from "../../types/index.types";
+import { CSVParser, TargetEmailForCSV } from "../../utils/csv.utils";
+import { logger } from "../../utils/logger.utils";
+import { SendMail } from "../../utils/mail.util";
+import { prisma } from "../../utils/prisma.utils";
+
+const GetPopupCSV = async (_req: Request, res: Response): Controller => {
+  const email = TargetEmailForCSV;
+  const scope = "Popup";
+
+  try {
+    const query = await prisma.popupCity.findMany();
+
+    if (!query?.length) {
+      return res.send(`No entry has been submitted for ${scope} yet!`);
+    }
+
+    if (query?.length) {
+      const csv = await CSVParser(query);
+
+      const response = await SendMail({
+        subject: `${scope} Submissions - From ETH Enugu`,
+        text: `Hello,\nHere is your requested attatchment`,
+        to: email,
+        attachments: [
+          {
+            filename: `${scope} - ETH Enugu ${new Date().toLocaleTimeString()}.csv`,
+            content: csv,
+          },
+        ],
+      });
+
+      if (response) {
+        return res.send(`${scope} applications have been sent to ${email}`);
+      }
+    }
+  } catch (error) {
+    logger.error(`Failed to send ${scope} CSV:`, error);
+
+    res.status(500).json({
+      success: false,
+      message: `Failed to send ${scope} CSV to ${email} `,
+      error,
+    });
+  }
+};
+
+export default GetPopupCSV;
